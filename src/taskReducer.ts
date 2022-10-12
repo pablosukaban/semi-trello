d44abd8d740bb1fb184185c614f61397d20e5956
ilya <zapaska96@gmail.com>
2022-10-12 21:19:35 +0500

    title

--- src/taskReducer.ts
+++ src/taskReducer.ts
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 import { TaskType } from './components/Column';
 
-export type BoardType = {
+export type ColumnType = {
     id: string;
     title: string;
     itemsList: TaskType[];
@@ -12,6 +12,8 @@ export enum BoardActions {
     BOARD_REMOVED = 'board_removed',
     TASK_ADDED = 'task_added',
     TASK_FINISHED = 'task_finished',
+    TITLE_CHANGED = 'title_changed',
+    TASK_REORDERED = 'task_reordered',
 }
 
 type AddBoardAction = {
@@ -34,13 +36,29 @@ type FinishTaskAction = {
     taskId: string;
 };
 
+type TaskReorderedAction = {
+    type: BoardActions.TASK_REORDERED;
+    column: ColumnType;
+    currentColumn: ColumnType;
+    task: TaskType;
+    currentTask: TaskType;
+};
+
+type ChangeTitleAction = {
+    type: BoardActions.TITLE_CHANGED;
+    title: string;
+    columnId: string;
+};
+
 export type BoardActionTypes =
     | AddBoardAction
     | RemoveBoardAction
     | AddTaskAction
-    | FinishTaskAction;
+    | FinishTaskAction
+    | TaskReorderedAction
+    | ChangeTitleAction;
 
-export const initialTasks: BoardType[] = [
+export const initialTasks: ColumnType[] = [
     {
         id: uuidv4(),
         title: 'Доска № 1',
@@ -59,29 +77,28 @@ export const initialTasks: BoardType[] = [
             { id: uuidv4(), value: 'Task 6', done: false },
         ],
     },
-    {
-        id: uuidv4(),
-        title: 'Доска № 3',
-        itemsList: [
-            { id: uuidv4(), value: 'Task 7', done: false },
-            { id: uuidv4(), value: 'Task 8', done: false },
-            { id: uuidv4(), value: 'Task 9', done: false },
-        ],
-    },
+    // {
+    //     id: uuidv4(),
+    //     title: 'Доска № 3',
+    //     itemsList: [
+    //         { id: uuidv4(), value: 'Task 7', done: false },
+    //         { id: uuidv4(), value: 'Task 8', done: false },
+    //         { id: uuidv4(), value: 'Task 9', done: false },
+    //     ],
+    // },
 ];
 
 export const taskReducer = (
-    state: BoardType[],
+    state: ColumnType[],
     action: BoardActionTypes
-): BoardType[] => {
+): ColumnType[] => {
     switch (action.type) {
         case BoardActions.BOARD_ADDED: {
             return [
                 ...state,
-
                 {
                     id: uuidv4(),
-                    title: `Доска № ${state.length}`,
+                    title: `Доска № ${state.length + 1}`,
                     itemsList: [
                         { id: uuidv4(), value: 'Task 7', done: false },
                         { id: uuidv4(), value: 'Task 8', done: false },
@@ -122,6 +139,34 @@ export const taskReducer = (
                     };
                 }
                 return column;
+            });
+        }
+
+        case BoardActions.TITLE_CHANGED:
+            return state.map((column) => {
+                if (column.id === action.columnId) {
+                    return { ...column, title: action.title };
+                }
+                return column;
+            });
+
+        case BoardActions.TASK_REORDERED: {
+            const { column, currentColumn, currentTask, task } = action;
+
+            const currentIndex = currentColumn.itemsList.indexOf(currentTask);
+            currentColumn.itemsList.splice(currentIndex, 1);
+
+            const dropIndex = column.itemsList.indexOf(task);
+            column.itemsList.splice(dropIndex + 1, 0, currentTask);
+
+            return state.map((c) => {
+                if (c.id === column.id) {
+                    return column;
+                }
+                if (c.id === currentColumn.id) {
+                    return currentColumn;
+                }
+                return c;
             });
         }
 
